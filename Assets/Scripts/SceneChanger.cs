24937c6ef7fd491783b5f06f43c4443582af2544
Tomasz Kądziołka <tkadziolka@MacBook-Pro-Tomasz.local>
2022-06-04 14:20:51 +0200

    Created play navigation

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,19 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
-    public void Scene1()
+    public void ChangeScene(string SceneName)
     {
-        SceneManager.LoadScene("Scene1");
-    }
-    public void Scene2()
-    {
-        SceneManager.LoadScene("Scene2");
-    }
-    public void Scene3()
-    {
-        SceneManager.LoadScene("Scene3");
+        SceneManager.LoadScene(SceneName);
     }
 }
