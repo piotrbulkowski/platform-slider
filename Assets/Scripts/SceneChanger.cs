f07b21f35bb53e408aa2fd6faa9ac11942b9a8f7
Tomasz Kądziołka <tkadziolka@MacBook-Pro-Tomasz.local>
2022-06-04 10:33:03 +0200

    Created home scene

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChanger : MonoBehaviour
+{
+    public void Scene1()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+    public void Scene2()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+    public void Scene3()
+    {
+        SceneManager.LoadScene("Scene3");
+    }
+}
