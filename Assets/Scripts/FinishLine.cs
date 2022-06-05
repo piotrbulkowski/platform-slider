3dd66c8273c99c81d2f4bdcb1110b131f8d3060d
Tomasz Kądziołka <tkadziolka@MacBook-Pro-Tomasz.local>
2022-06-05 12:15:11 +0200

    Completed finish line mechanism

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,13 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Finished ride");
+        if (collision.tag == "Player")
+        {
+            float playerTime = PlayerPrefs.GetFloat("Player Score");
+            float elapsedTime = Time.timeSinceLevelLoad;
+
+            Debug.Log($"Finish time {elapsedTime}");
+
+            // First save
+            if (playerTime == 0.0)
+            {
+                PlayerPrefs.SetFloat("Player Score", elapsedTime);
+            }
+
+            // Update when better
+            if (elapsedTime > 0.0 && elapsedTime < playerTime)
+            {
+                UpdateBestTimeCounter(elapsedTime);
+                PlayerPrefs.SetFloat("Player Score", elapsedTime);
+            }
+
+            Debug.Log("Ride finished");
+            PauseGame(); // TODO And navigate to the next level
+        }
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    void UpdateBestTimeCounter(float time)
+    {
+        GameObject bestScoreObject = GameObject.Find("BestTimeScore");
+        TextMeshProUGUI bestScore = bestScoreObject.GetComponent<TextMeshProUGUI>();
+        if (bestScore != null)
+        {
+            bestScore.text = $"Best: {time}s";
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,26 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿using TMPro;
+using UnityEngine;
 
 public class Player : MonoBehaviour
 {
 
     public Rigidbody2D rb;
+
+    void Start()
+    {
+        float bestResult = PlayerPrefs.GetFloat("Player Score", 0.0f);
+
+        GameObject bestScoreObject = GameObject.Find("BestTimeScore");
+        TextMeshProUGUI bestScore = bestScoreObject.GetComponent<TextMeshProUGUI>();
+        if (bestScore != null)
+        {
+            bestScore.text = $"Best: {bestResult}s";
+        }
+        else
+        {
+            Debug.Log("Best score object is null");
+        }
+    }
 
     // Update is called once per frame
     void Update()
@@ -14,9 +30,15 @@ public class Player : MonoBehaviour
             rb.bodyType = RigidbodyType2D.Dynamic;
         }
 
-        // TODO Check if canvas not breaks reference
-        Debug.Log("Started");
-        Text myText = GameObject.Find("CurrentTimeScore").GetComponent<Text>();
-        myText.text = "Time Since Loaded : " + Time.timeSinceLevelLoad;
+        GameObject bestScoreObject = GameObject.Find("CurrentTimeScore");
+        TextMeshProUGUI currentScore = bestScoreObject.GetComponent<TextMeshProUGUI>();
+        if (currentScore != null)
+        {
+            currentScore.text = $"Current: {Time.timeSinceLevelLoad}s";
+        }
+        else
+        {
+            Debug.Log("Current score object is null");
+        }
     }
 }
