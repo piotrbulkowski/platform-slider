737aa98ebcf06e88dd1f7709c17d72eea5662ea7
Tomasz Kądziołka <tkadziolka@MacBook-Pro-Tomasz.local>
2022-06-07 20:18:33 +0200

    Created resfresh screen

---
+++ Assets/Scripts/RefreshScene.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RefreshScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+        ResumeGame();
+    }
+}
